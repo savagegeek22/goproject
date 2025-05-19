eae8a5fb957e18990e47e2aa9edb51e3051d7ac3
JD Henderson <jdhenderson@savagegeek.net>
2025-05-19 12:11:25 -0400

    Update hi.go to add new route

--- hi.go
+++ hi.go
@@ -1,25 +1,19 @@
-// File: main.go
+// hi.go
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
-type Message struct {
-    Greeting string `json:"greeting"`
-}
-
-func greetHandler(w http.ResponseWriter, r *http.Request) {
-    msg := Message{Greeting: "Hello, Golang Developer!"}
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(msg)
-}
-
 func main() {
-    http.HandleFunc("/greet", greetHandler)
-    log.Println("Server running on http://localhost:8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
-}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, Go web!")
+	})
 
+	fmt.Println("Server running on http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		panic(err)
+	}
+}
